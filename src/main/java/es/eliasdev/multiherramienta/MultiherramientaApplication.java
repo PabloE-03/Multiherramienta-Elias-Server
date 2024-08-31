ee4320111c72c9ecd0bd7b5ed3b9f62215a2f26f
PabloE-03 <peibol.max@gmail.com>
2024-08-31 12:41:55 +0200

    Estructura del proyecto

---
+++ src/main/java/es/eliasdev/multiherramienta/MultiherramientaApplication.java
@@ -0,0 +1,13 @@
+package es.eliasdev.multiherramienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiherramientaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiherramientaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/eliasdev/multiherramienta/MultiherramientaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.eliasdev.multiherramienta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiherramientaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
