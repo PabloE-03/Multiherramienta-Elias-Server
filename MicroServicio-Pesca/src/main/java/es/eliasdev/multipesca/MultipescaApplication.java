477384135a89a7100c21b7ee4c490b5038a89168
PabloE-03 <peibol.max@gmail.com>
2024-09-02 10:36:01 +0200

    Reestructuracion de los microservicios

---
+++ MicroServicio-Pesca/src/main/java/es/eliasdev/multipesca/MultipescaApplication.java
@@ -0,0 +1,13 @@
+package es.eliasdev.multipesca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipescaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipescaApplication.class, args);
+	}
+
+}
---
+++ MicroServicio-Pesca/src/test/java/es/eliasdev/multipesca/MultipescaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.eliasdev.multipesca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipescaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
